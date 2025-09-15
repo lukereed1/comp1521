c33bc5fe2901da92fb599e6ff9a22b422c16a150
Luke Reed <luke.reed@outlook.com.au>
2025-09-16 08:45:31 +1000

    pushing

--- lab01/no_vowels.c
+++ lab01/no_vowels.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
+
+int isVowel(char c);
 
 int main(void) {
-	return 0;
+	char c;
+
+    while (scanf("%c", &c) == 1) {
+        if (!isVowel(c)) {
+            printf("%c", c);
+        }
+    }
+
+    return 0;
+}
+
+int isVowel(char c) {
+    if (
+        tolower(c) == 'a' || tolower(c) == 'e' || tolower(c) == 'i' ||
+        tolower(c) == 'o' || tolower(c) == 'u') {
+            
+        return 1;
+    }
+
+    return 0;
 }
