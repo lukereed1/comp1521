7e19faf140abc95133b046c35b032e53c79823e0
Luke Reed <luke.reed@outlook.com.au>
2025-09-16 09:19:18 +1000

    pushing

--- lab01/no_odd_lines.c
+++ lab01/no_odd_lines.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 1024
+
 int main(void) {
-	return 0;
+	char s[MAX];
+
+    while (fgets(s, MAX, stdin) != NULL) {
+        int length = strlen(s);
+        if (length % 2 == 0) {            
+            fputs(s, stdout);
+        }        
+    }
 }
