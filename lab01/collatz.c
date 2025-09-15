d35d25f24e7584daefb62694755a350a11d82e36
Luke Reed <luke.reed@outlook.com.au>
2025-09-16 09:52:01 +1000

    pushing

--- lab01/collatz.c
+++ lab01/collatz.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void doCollatz(int num);
+
 int main(int argc, char *argv[])
 {
-	(void) argc, (void) argv; // keep the compiler quiet, should be removed
-	return EXIT_SUCCESS;
+    if (argc != 2) {
+        printf("Usage: ./collatz NUMBER\n");
+        return 0;
+    }
+
+    doCollatz(atoi(argv[1]));
+
+    return 0;
+}
+
+void doCollatz(int num) {
+    printf("%d\n", num);
+
+    if (num == 1) {
+        return;
+    }
+
+    if (num % 2 == 0) {
+        doCollatz(num / 2);
+    } else {
+        doCollatz(3 * num + 1);
+    }
 }
