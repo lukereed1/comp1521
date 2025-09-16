cd61efe7dfe4ef1475a90a240902e6eb9e0e0438
Luke Reed <luke.reed@outlook.com.au>
2025-09-16 11:05:06 +1000

    pushing

--- lab01/gaussian_sum.s
+++ lab01/gaussian_sum.s
@@ -18,18 +18,60 @@
 # }
 
 main:
-    la      $a0, prompt1
-    li      $v0, 4
-    syscall
+	li	$v0, 4
+	la	$a0, prompt1
+	syscall
 
-    li      $v0, 0
-    jr      $ra          # return
+	li	$v0, 5
+	syscall
+	move 	$t0, $v0
+
+	li	$v0, 4
+	la	$a0, prompt2
+	syscall
+	
+	li	    $v0, 5
+	syscall
+	move	$t1, $v0
+
+	SUB 	$t2, $t1, $t0
+	ADD	    $t3, $t1, $t0
+	ADD	    $t4, $t2, 1
+	MUL 	$t3, $t4, $t3
+	DIV 	$t3, $t3, 2
+	
+	li	$v0, 4
+	la	$a0, answer1
+	syscall
+
+	li 	$v0, 1
+	move	$a0, $t0
+	syscall
+
+	li	$v0, 4
+	la	$a0, answer2
+	syscall
+
+	li 	$v0, 1
+	move 	$a0, $t1
+	syscall
+
+	li	$v0, 4
+	la	$a0, answer3
+	syscall
+
+	li	$v0, 1
+	move 	$a0, $t3
+	syscall
+
+	li	$v0, 0
+	jr	$ra
 
 
 .data
-    prompt1: .asciiz "Enter first number: "
-    prompt2: .asciiz "Enter second number: "
+	prompt1: .asciiz "Enter first number: "
+	prompt2: .asciiz "Enter second number: "
 
-    answer1: .asciiz "The sum of all numbers between "
-    answer2: .asciiz " and "
-    answer3: .asciiz " (inclusive) is: "
+	answer1: .asciiz "The sum of all numbers between "
+	answer2: .asciiz " and "
+	answer3: .asciiz " (inclusive) is: "
