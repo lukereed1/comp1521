47a1bc46350f89cf76d21c47c193a8274403f58c
Luke Reed <luke.reed@outlook.com.au>
2025-09-16 10:17:21 +1000

    pushing

--- lab01/fibonacci.c
+++ lab01/fibonacci.c
@@ -1,8 +1,33 @@
+// fibonacci.c
+// Written by Luke Reed (z5677864)
+// on 16/09/2025
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SERIES_MAX 30
 
+int doFibonacci(int num);
+
 int main(void) {
-    return EXIT_SUCCESS;
+    int num;
+
+    while (scanf("%d", &num) == 1) {
+        int result = doFibonacci(num);
+        printf("%d\n", result);
+    }
+
+    return 0;
+}
+
+int doFibonacci(int num) {
+    if (num == 0) {
+        return 0;
+    }
+
+    if (num == 1) {
+        return 1;
+    }
+
+    return doFibonacci(num - 1) + doFibonacci(num - 2);
 }
