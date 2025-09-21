dc0e0b97b14fee2b50919e191dc4fe8b97f70c7f
Luke Reed <z5677864@ad.unsw.edu.au>
2025-09-21 14:01:06 +1000

    pushing

---
+++ exercises/w1/sum_100_squares.s
@@ -0,0 +1,32 @@
+PRINT_INT = 1
+PRINT_STRING = 4
+
+	# sum = $t0
+	# i = $t1
+
+
+	.text
+main:
+	li	$t0, 0
+
+loop_init:
+	li	$t1, 0
+
+loop_condition:
+	BGT	$t1, 100, loop_end
+
+loop_body:
+	MUL	$t2, $t1, $t1
+	ADD 	$t0, $t2
+	
+loop_step:
+	ADDI	$t1, 1
+	B	loop_condition
+
+loop_end:
+	li	$v0, PRINT_INT
+	MOVE	$a0, $t0
+	syscall
+
+	li 	$v0, 0
+	jr	$ra	
